07e9a425d2ad8dc8f96d6ff78f87d5d30f3375d6
mizuky fujitani <mizuki.fujitani@gmail.com>
2016-11-23 12:56:44 +0900

    CommandLineBuilderのドキュメンテーション・コメントを追加

--- Unclazz.Commons.CLI/CommandLineBuilder.cs
+++ Unclazz.Commons.CLI/CommandLineBuilder.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 
 namespace Unclazz.Commons.CLI
 {
+	/// <summary>
+	/// コマンドラインの定義を組み立てるビルダーです。
+	/// ビルダーのインスタンスは<see cref="CommandLine.Builder(string)"/>メソッドを通じて得られます。
+	/// </summary>
 	public class CommandLineBuilder
 	{
 		private static readonly Action<IEnumerable<string>> noop = (ss) => { };
@@ -22,20 +26,39 @@ namespace Unclazz.Commons.CLI
 			commandName = cn;
 		}
 
+		/// <summary>
+		/// コマンドの説明を設定します。
+		/// </summary>
+		/// <param name="d">コマンドの説明</param>
 		public CommandLineBuilder Description(string d)
 		{
 			description = d == null ? string.Empty : d;
 			return this;
 		}
+		/// <summary>
+		/// コマンドラインのパースにおいて大文字小文字を区別するかどうかを設定します。
+		/// デフォルトは<c>true</c>です。
+		/// </summary>
+		/// <returns>ビルダー</returns>
+		/// <param name="cs"><c>true</c>の場合は大文字小文字を区別する</param>
 		public CommandLineBuilder CaseSensitive(bool cs)
 		{
 			caseSensitive = cs;
 			return this;
 		}
+		/// <summary>
+		/// コマンドラインのパースにおいて大文字小文字を区別するように設定します。
+		/// </summary>
+		/// <returns>ビルダー</returns>
 		public CommandLineBuilder CaseSensitive()
 		{
 			return CaseSensitive(true);
 		}
+		/// <summary>
+		/// コマンドライン・オプションの定義を追加します。
+		/// </summary>
+		/// <returns>ビルダー</returns>
+		/// <param name="o">コマンドライン・オプションの定義</param>
 		public CommandLineBuilder AddOption(IOption o)
 		{
 			if (o == null)
@@ -45,15 +68,33 @@ namespace Unclazz.Commons.CLI
 			options.Add(o);
 			return this;
 		}
+		/// <summary>
+		/// コマンドライン・オプションの定義を追加します。
+		/// このメソッドはオプションを組み立てるためのビルダーへの参照を返します。
+		/// </summary>
+		/// <returns>ビルダー</returns>
+		/// <param name="n">オプションの名前</param>
 		public OptionBuilder AddOption(string n)
 		{
 			return new OptionBuilder(this, n);
 		}
+		/// <summary>
+		/// コマンドライン引数のうちコマンドライン・オプションの定義情報に
+		/// 含まれなかった「残りの引数」を処理するためのデリゲートを設定します。
+		/// デフォルト値として利用されているデリゲートは受け取った値を単に破棄します。
+		/// </summary>
+		/// <returns>ビルダー</returns>
+		/// <param name="setter">デリゲート</param>
 		public CommandLineBuilder SetterDelegate(Action<IEnumerable<string>> setter)
 		{
 			setterDelegate = setter == null ? noop : setter;
 			return this;
 		}
+		/// <summary>
+		/// コマンドラインの定義を組み立てます。
+		/// </summary>
+		/// <returns>コマンドラインの定義</returns>
+		/// <exception cref="ApplicationException">オプションの定義が1つも指定されていない場合</exception>
 		public ICommandLine Build()
 		{
 			if (options.Count == 0)
@@ -64,6 +105,4 @@ namespace Unclazz.Commons.CLI
 			                       options, setterDelegate);
 		}
 	}
-
-
 }
